4243680da2a7d85390c4b9936fdec4c3fe0e774f
Mike Metzger <mdmetzger@gmail.com>
2011-02-03 22:31:58 -0600

    Modified the detailView dataDetectorTypes to look for all types.

--- Classes/DetailViewController.m
+++ Classes/DetailViewController.m
@@ -43,7 +43,8 @@
 	htmldata = [htmldata stringByAppendingString:@"<br /><br /><br />"];
 //	NSString *html = @"<html><body>This tool will help you read calendar files from various sources.  Opening it directly won't help significantly as it will automatically read any linked .ics files from applications that support document reading (DropBox, Mail (including before 4.2), Safari, and so forth.)  <p>Regarding ICS files in Mail.app - if like most users, you want to read calendar invitations that you receive via e-mail.  Unfortunately, versions of iOS prior to 4.2 have an issue properly handling these attachments.  It doesn't even properly recognize that these attachments can be interacted with.  We have discovered a workaround that makes opening these files possible.<p>To workaround this issue, forward the e-mail containing the calendar invite to resend@icsreader.com.  The server will reply with the calendar attachment in a form that can be opened by ICS Reader.  <b>NOTE: This is not required if you're running iOS 4.2 or later.</b><p>To open the attachment, press and hold on the icon for the attachment and a window will pop up allowing you to open the attachment in ICS Reader.  Press the ICS Reader and it will parse the file for you, showing the details and giving you the option of saving the appointment to your calendar.<p>&nbsp;</p><p>&nbsp;</p></body></html>";
 	[detailView loadHTMLString:htmldata baseURL:[NSURL URLWithString:@"http://www.icsreader.com"]];
-    [super viewDidLoad];
+    detailView.dataDetectorTypes = UIDataDetectorTypeAll;
+	[super viewDidLoad];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
